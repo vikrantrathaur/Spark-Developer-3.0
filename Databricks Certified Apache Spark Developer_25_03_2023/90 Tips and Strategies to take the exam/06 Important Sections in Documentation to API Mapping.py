b74f923932e09edbb33f7ca3c9e21ec2353c5c6c
vikrantrathaur <89038721+vikrantrathaur@users.noreply.github.com>
2023-03-26 01:19:47 +0530

    Delete Databricks Certified Apache Spark Developer_25_03_2023 directory

--- Databricks Certified Apache Spark Developer_25_03_2023/90 Tips and Strategies to take the exam/06 Important Sections in Documentation to API Mapping.py
+++
@@ -1,5 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC 
-# MAGIC Here is the mapping between important sections in provided documentation to API Mapping.
-# MAGIC * `pyspark.sql.functions`
