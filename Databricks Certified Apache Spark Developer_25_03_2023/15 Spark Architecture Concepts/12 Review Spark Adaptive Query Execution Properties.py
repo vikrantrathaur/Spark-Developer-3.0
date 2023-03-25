40bc1ceed18210214c7219735d12951d3b83c2c4
vikrantrathaur <89038721+vikrantrathaur@users.noreply.github.com>
2023-03-26 01:19:47 +0530

    Delete Databricks Certified Apache Spark Developer_25_03_2023 directory

--- Databricks Certified Apache Spark Developer_25_03_2023/15 Spark Architecture Concepts/12 Review Spark Adaptive Query Execution Properties.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC 
-# MAGIC There are quite a few properties related to Adaptive execution. We can go to official documentation of Spark and review the details related to adaptive execution.
-
-# COMMAND ----------
-
-
